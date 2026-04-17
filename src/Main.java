bc3c69be85a550aa1e4c4794b6ee459165aba687
ddwq23143 <alanov.danila.12@gmail.com>
2026-04-17 20:10:29 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        Start start = new Start();
+        start.start();
+    }
+}
